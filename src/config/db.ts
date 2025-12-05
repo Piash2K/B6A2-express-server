4fa28755e5cc0f1fe16dac438cb27d512084f3d1
PIASH <zmnobab1999@gmail.com>
2025-12-05 23:52:46 +0600

    create vehicles table

--- src/config/db.ts
+++ src/config/db.ts
@@ -6,7 +6,7 @@ export const pool = new Pool({
 });
 
 const initDB = async () => {
-  await pool.query(`CREATE TABLE Users(
+  await pool.query(`CREATE TABLE IF NOT EXISTS Users(
     id SERIAL PRIMARY KEY,
     name VARCHAR(150) NOT NULL,
     email VARCHAR(200) UNIQUE NOT NULL CHECK (email = LOWER(email)),
@@ -14,6 +14,15 @@ const initDB = async () => {
     phone VARCHAR(15) NOT NULL,
     role VARCHAR(50) NOT NULL CHECK (role = 'admin' OR role = 'customer')
     )`);
+
+  await pool.query(`CREATE TABLE IF NOT EXISTS Vehicles(
+        id SERIAL PRIMARY KEY,
+        vehicle_name VARCHAR(150) NOT NULL,
+        type VARCHAR(50) NOT NULL CHECK(type IN ('car', 'bike', 'van','SUV')),
+        registration_number VARCHAR(200) UNIQUE NOT NULL,
+        daily_rent_price INT NOT NULL CHECK(daily_rent_price>0),
+        availability_status VARCHAR(100) NOT NULL CHECK (availability_status IN ('available','booked'))
+        )`);
 };
 
 export default initDB;
